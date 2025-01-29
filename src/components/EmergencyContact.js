ae32b56de54489174193b1c8d6a7c3c7936e4807
hpourali <h.pourali.a@gmail.com>
2025-01-29 10:06:24 +0330

    Change SOS button.

--- src/components/EmergencyContact.js
+++ src/components/EmergencyContact.js
@@ -18,17 +18,18 @@ const EmergencyContact = () => {
         className="sos-button" 
         onClick={handleButtonClick}
       >
-        درخواست کمک اضطراری
+        SOS
       </button>
       {showContacts && (
         <div className="contact-list">
           <button className="close-button" onClick={handleCloseClick}>&times;</button>
           <p>در مواقع اضطراری، می‌توانید با شماره‌های زیر تماس بگیرید:</p>
           <ul>
-            <li><strong>اورژانس اجتماعی:</strong> <a href="tel:123" className="phone-number">123</a> - کنارت هستیم.</li>
-            <li><strong>صدای مشاور:</strong> <a href="tel:1480" className="phone-number">1480</a> - یک تماس ساده می‌تواند آغاز مسیر بهبودی باشد.</li>
-            <li><strong>اورژانس روانپزشکی تهران:</strong> <a href="tel:44508200" className="phone-number">44508200</a> - متخصصان ما ۲۴ ساعته آماده ارائه خدمات تخصصی روانپزشکی هستند. سلامت روان شما اولویت ماست.</li>
+            <li><strong>اورژانس اجتماعی:</strong> <a href="tel:123" className="phone-number">123</a></li>
+            <li><strong>صدای مشاور:</strong> <a href="tel:1480" className="phone-number">1480</a></li>
+            <li><strong>اورژانس روانپزشکی تهران:</strong> <a href="tel:44508200" className="phone-number">44508200</a></li>
           </ul>
+          <strong><p className="supportive-message">کنارت هستیم.</p></strong>
         </div>
       )}
     </div>
